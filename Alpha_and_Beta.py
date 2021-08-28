c07f2b7664e0f3cce7b101117f816ec7ee1d2876
eisenmsi <42298986+eisenmsi@users.noreply.github.com>
2021-08-28 12:43:08 +0200

    Update Alpha_and_Beta.py
    Fix problem

--- Alpha_and_Beta.py
+++ Alpha_and_Beta.py
@@ -5,7 +5,7 @@ from scipy import stats
 def alpha_beta_portfolio(stock, weights, year, duration=5):
     begin = year - duration
     start = str(begin) + "-12-25"
-    end = str(year + 1) + "-01-05"
+    end = str(year) + "-12-25"
     price_data = web.get_data_yahoo(stock, start, end)
     ret_data = price_data['Adj Close'].resample("M").ffill().pct_change()[1:]
     port_ret = (ret_data * weights).sum(axis=1)
@@ -18,7 +18,7 @@ def alpha_beta_portfolio(stock, weights, year, duration=5):
 def alpha_beta_stock(stock, year, duration=5):
     begin = year - duration
     start = str(begin) + "-12-25"
-    end = str(year + 1) + "-01-05"
+    end = str(year) + "-12-25"
     price_data = web.get_data_yahoo(stock, start, end)
     ret_data = price_data['Adj Close'].resample("M").ffill().pct_change()[1:]
     benchmark_price = web.get_data_yahoo('SPY', start, end)
